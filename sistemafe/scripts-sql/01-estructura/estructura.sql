6c1c3b37c5db54d1b72886971f81d67a9db0d276
juntosbuscandote <50184575+juntosbuscandote@users.noreply.github.com>
2019-06-15 00:44:23 -0500

    a
    a

--- sistemafe/scripts-sql/01-estructura/estructura.sql
+++
@@ -1,8 +0,0 @@
-/*Eliminar el unique*/
-ALTER TABLE t_employee DROP CONSTRAINT t_employee_uq
-/*Cambiar tipo de dato al store_bstate*/
-ALTER TABLE t_storehouse ALTER COLUMN store_bstate TYPE character varying(2)
-/*mtoledo 18/02/2018 Relación con de usuarios y empleados, obteniendo la empresa relacionada*/
-ALTER TABLE t_employee ADD user_vuser varchar(50);
-COMMENT ON COLUMN t_employee.user_vuser 
-     IS 'Relación con de usuarios'
